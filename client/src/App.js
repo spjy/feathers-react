34c1682208ffcb4876c6eb2707bead644a44de82
Spencer <spjy@hawaii.edu>
2018-01-09 19:19:12 -1000

    Fixed login error

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import feathers from '@feathersjs/feathers';
 import rest from '@feathersjs/rest-client';
 import auth from '@feathersjs/authentication-client';
+import socketio from '@feathersjs/socketio-client';
+import io from 'socket.io-client';
 //import localStorage from 'feathers-localstorage';
 
 import logo from './logo.svg';
 import './App.css';
 
 const app = feathers();
-app.configure(auth(app.get('auth')));
+app.configure(auth({
+  storageKey: 'authKey',
+  storage: window.localStorage
+}));
+
+const socket = io('http://localhost:3030');
+
+app.configure(socketio(socket))
+
 const restClient = rest('http://localhost:3030');
 
 class App extends Component {
@@ -20,16 +30,26 @@ class App extends Component {
   }
 
   componentDidMount() {
-    app.configure(restClient.fetch(window.fetch));
+    //app.configure(restClient.fetch(window.fetch));
 
-    const messages = app.service('messages');
-    messages.find().then(res => {
-      console.log(res)
-      this.setState({ messages: res.data })
-    });
+    const { authKey } = window.localStorage;
 
-    app.authenticate().then(res => {
-      console.log(res)
+    console.log(authKey)
+
+    app.authenticate({
+      strategy: 'jwt',
+      accessToken: authKey
+    }).then(response => {
+      const messages = app.service('messages');
+
+      messages.find().then(res => {
+        console.log(res)
+        this.setState({ messages: res.data })
+      });
+
+      socket.on('messages created', message => {
+        console.log(message);
+      })
     }).catch(err => {
       console.log(err)
     });
@@ -43,6 +63,14 @@ class App extends Component {
       password: this.state.password
     }).then(res => {
       console.log('logged in breh', res)
+      const messages = app.service('messages');
+
+      messages.find().then(res => {
+        console.log(res)
+        this.setState({ messages: res.data })
+      });
+      app.service('authentication').create({ Authorization: res.accessToken })
+
       return app.passport.verifyJWT(res.accessToken);
     }).catch(err => {
       console.error('Authentication error', err);
@@ -57,15 +85,29 @@ class App extends Component {
     this.setState({ password: e.target.value });
   }
 
+  newMessage(e) {
+    e.preventDefault();
+    console.log(e.target.msg.value)
+    app.service('messages').create({ text: e.target.msg.value });
+  }
+
+  logout(e) {
+    app.logout();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <button>Logout</button>
+          <h1 className="App-title">Feathers</h1>
+          <button onClick={this.logout.bind(this)}>Logout</button>
         </header>
         <br />
+        <form action="" onSubmit={this.newMessage.bind(this)}>
+          <input id="msg" type="text" />
+          <button type="submit">Enter</button>
+        </form>
         {this.state.messages.length !== 0 ? this.state.messages.map((message, i) => {
           return (
             <div key={i}>{message.text}</div>
